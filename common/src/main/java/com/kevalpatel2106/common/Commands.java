c3ce9db216f5dcfb28586a5cb4f4e96893ca4a8d
Keval Patel <kevalpatel2106@gmail.com>
2017-05-14 11:26:25 +0530

    Adding commands list

---
+++ common/src/main/java/com/kevalpatel2106/common/Commands.java
@@ -0,0 +1,15 @@
+package com.kevalpatel2106.common;
+
+/**
+ * Created by Keval Patel on 14/05/17.
+ *
+ * @author 'https://github.com/kevalpatel2106'
+ */
+
+public class Commands {
+    public static final String TURN_LEFT = "left";
+    public static final String TURN_RIGHT = "right";
+    public static final String MOVE_FORWARD = "forward";
+    public static final String MOVE_REVERSE = "reverse";
+    public static final String STOP = "stop";
+}
