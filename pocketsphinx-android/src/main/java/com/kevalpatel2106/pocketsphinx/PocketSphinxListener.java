7ce701ba5c9bdcca3bb6d7a506815989a109bcaf
Keval <kevalpatel2106@gmail.com>
2017-05-22 16:08:15 +0530

    Adding speech recognition module

--- pocketsphinx-android/src/main/java/com/kevalpatel2106/pocketsphinx/PocketSphinxListener.java
+++ pocketsphinx-android/src/main/java/com/kevalpatel2106/pocketsphinx/PocketSphinxListener.java
@@ -14,4 +14,18 @@
  *  limitations under the License.
  */
 
-include ':things', ':tensorflow-android-inference', ':pocketsphinx-android'
+package com.kevalpatel2106.pocketsphinx;
+
+/**
+ * Created by Keval on 05-May-17.
+ */
+
+public interface PocketSphinxListener {
+    void onSpeechRecognizerReady();
+
+    void onActivationPhraseDetected();
+
+    void onTextRecognized(String recognizedText);
+
+    void onTimeout();
+}
